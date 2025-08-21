ef374f1580f70e82f4e394b00a9c20cb144f91f3
Cullen Gray <cullen@42prague.com>
2025-08-22 01:46:58 +0200

    copy from ex02

---
+++ ex03/ex03.c
@@ -0,0 +1,258 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Color definitions
+#define BOLD     "\033[1m"
+#define BLACK    "\033[30m"
+#define RED      "\033[31m"
+#define GREEN    "\033[32m"
+#define WHITE    "\033[37m"
+#define CYAN     "\033[36m"
+#define RESET    "\033[0m"
+
+// Background color definitions
+#define BG_BLACK       "\033[40m"
+#define BG_RED         "\033[41m"
+#define BG_GREEN       "\033[42m"
+
+typedef struct s_arena
+{
+	void	*mem;
+	size_t	size;
+	size_t	offset;
+} t_arena;
+
+typedef struct s_map
+{
+	char		**m;
+	char        *name;
+	size_t		w, h;
+    size_t      px, py;
+	t_arena		arena;
+} t_map;
+
+
+void	free_map(t_map *map)
+{
+	if (map && map->arena.mem)
+	{
+		free(map->arena.mem);
+		map->arena = (t_arena){0};
+		map->m = NULL;
+	}
+}
+
+/* Prints error message, frees map and closes fd if needed */
+void	err_exit(const char *s, t_map *map, int fd)
+{
+	if (fd > 0) close(fd);
+	if (map) free_map(map);
+	write(2, s, strlen(s));
+	write(2, "\n", 1);
+	exit(1);
+}
+
+/* One malloc needed */
+t_arena	arena_init(size_t size)
+{
+	t_arena a = {malloc(size), size, 0};
+	if (!a.mem)
+		err_exit("alloc fail", NULL, 0);
+	return (a);
+}
+
+void	*alloc_arena(t_arena *a, size_t size)
+{
+	if (a->offset + size > a->size)
+		err_exit("Arena out of mem", NULL, 0);
+	void *ptr = a->mem + a->offset;
+	a->offset += size;
+	return (ptr);
+}
+
+int		is_wall(char c)
+{
+	return (c == '1');
+}
+
+const char *get_tile_display(char c)
+{
+	switch (c)
+	{
+		case '1': return BLACK "██" RESET;
+		case '0': return WHITE "██" RESET;
+		case 'P': return GREEN "██" RESET;
+		case 'E': return RED "██" RESET;
+		default:  return "??";
+	}
+}
+
+const char *get_tile_display_d(char c)
+{
+	switch (c)
+	{
+		case '1': return BG_BLACK "WW" RESET;
+		case '0': return "  ";
+		case 'P': return BG_GREEN "PP" RESET;
+		case 'E': return BG_RED "EE" RESET;
+		default:  return "??";
+	}
+}
+
+void	print_map(t_map *map)
+{
+	size_t name_len = strlen(map->name) - 4;
+	size_t display_width = map->w * 2;  // Each tile is 2 chars wide
+	size_t padding = (display_width + 2 > name_len) ? (display_width + 2 - name_len) / 2 : 0;
+	
+	// Print centered title
+	printf(BOLD CYAN "%*s%.*s\n" RESET, (int)padding, "", (int)name_len, map->name);
+	printf("╔");
+	for (size_t i = 0; i < display_width; i++)
+		printf("═");
+	printf("╗\n");
+	
+	for (size_t i = 0; i < map->h; i++)
+	{
+		printf("║");
+		for (size_t j = 0; j < map->w; j++)
+			printf("%s", get_tile_display_d(map->m[i][j]));
+		printf("║\n");
+	}
+	
+	printf("╚");
+	for (size_t i = 0; i < display_width; i++)
+		printf("═");
+	printf("╝\n");
+}
+
+/* Check top/bottom/left/right are enclosed */
+int		validate_walls(t_map *map)
+{
+	for (size_t i = 0; i < map->w; i++)
+	{
+		if (!is_wall(map->m[0][i]) || !is_wall(map->m[map->h - 1][i]))
+			return (err_exit("Bad horizontal walls", map, 0), 0);
+	}
+	
+	for (size_t j = 0; j < map->h; j++)
+		if (!is_wall(map->m[j][0]) || !is_wall(map->m[j][map->w - 1]))
+			return (err_exit("Bad vertical walls", map, 0), 0);
+	
+	return (1);
+}
+
+/* Alloc and copy into map */
+void	fill_map(const char *buf, t_map *map)
+{
+	map->m = alloc_arena(&map->arena, map->h * sizeof(char *));
+	for (size_t i = 0; i < map->h; i++)
+	{
+		map->m[i] = alloc_arena(&map->arena, map->w + 1);
+		memcpy(map->m[i], &buf[i * (map->w + 1)], map->w);
+		map->m[i][map->w] = '\0';
+	}
+	return ;
+}
+
+/* Get size of map, making sure it is rectangular */
+void	map_check(const char *buf, t_map *map)
+{
+	const char *line = strchr(buf, '\n');
+	if (!line)
+		return (err_exit("No newlines", map, 0));
+	
+	map->w = line - buf;
+	map->h = 0;
+	
+	line = buf;
+	while ((line = strchr(line, '\n')) != NULL)
+	{
+		map->h++;
+		line++;
+	}
+	// printf("w: %lu h: %lu\n", map->w, map->h);
+
+	line = buf;
+	for (size_t i = 0; i < map->h; i++)
+	{
+		const char *next_line = strchr(line, '\n');
+
+		size_t line_len;
+		if (next_line)
+			line_len = next_line - line;
+		else
+			line_len = strlen(line);
+
+		if (line_len != map->w)
+			return (err_exit("Inconsistent line lengths", map, 0));
+		if (next_line)
+			line = next_line + 1;
+	}
+	fill_map(buf, map);
+}
+
+int	ends_with_txt(const char *str, size_t n)
+{
+	size_t	len = strlen(str);
+
+	if (len < n)
+		return (0);	
+	return (strcmp(str + (len - n), ".txt") == 0);
+}
+
+/* Check filename and ability to open */
+int	file_check(char *filename)
+{
+	int fd;
+
+	if (!ends_with_txt(filename, 4))
+		return (err_exit("Wrong filetype", NULL, 0), 0);
+
+	fd = open(filename, O_RDONLY);
+	
+	if (fd < 0)
+		return (err_exit("Can't open", NULL, fd), 0);
+	
+	return fd;
+}
+
+/* Half arena is for file buf, other half is for program mem */
+void	read_map(char *filename, t_map *map)
+{
+	int		fd = file_check(filename);
+	map->name = strrchr(filename, '/');
+	if (!map->name)
+		map->name = filename;
+	else
+		map->name += 1;
+	
+	size_t	buf_size = map->arena.size / 2;
+	char	*buf = alloc_arena(&map->arena, map->arena.size / 2);
+	size_t	bytes = read(fd, buf, buf_size);
+	if (bytes <= 0)
+		return (err_exit("Can't read file", map, fd));
+	
+	buf[bytes] = '\0';
+	map_check(buf, map);
+	validate_walls(map);
+	
+	close(fd);
+}
+
+int	main(int ac, char *av[])
+{
+	if (ac == 2)
+	{
+		t_map map = {0};
+		map.arena = arena_init(1 << 16); //64kb
+
+		read_map(av[1], &map);
+		print_map(&map);
+		free_map(&map);
+	}
+	return 0;
+}
